ed5f88f291820921bfca2a92ec678a108913a9c8
jonny rhea <jonathan.rhea@gmail.com>
2020-02-04 15:43:32 -0600

    fmt mods

--- tests/ee_tests.rs
+++ tests/ee_tests.rs
@@ -1,8 +1,6 @@
 extern crate ease;
-use wrapper::{ee_code,transfer,build_state};
-use ease::primitives::address::Address;
 use ease::engine::Engine;
-
+use wrapper::{build_state, ee_code, transfer};
 
 #[test]
 fn test() {
@@ -14,13 +12,18 @@ fn test() {
     // get generated ee wasm code
     let code = ee_code();
     // deploy code with state
-    engine.deploy(code,state);
+    engine.deploy(code, state);
     // create a tx
-    let tx = transfer("zero".as_bytes().to_vec(),"one".as_bytes().to_vec(),100,1);
+    let tx = transfer(
+        "zero".as_bytes().to_vec(),
+        "one".as_bytes().to_vec(),
+        100,
+        1,
+    );
     // execute tx on node
     engine.run(tx);
     // update local state
     //state.get_mut(Address::zero).set_balance(0);
     //state.get_mut(Address::one).set_balance(200);
-    assert_eq!(1+1, 2);
+    assert_eq!(1 + 1, 2);
 }
