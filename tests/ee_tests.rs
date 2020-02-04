ef8563d98bc37f19c68c9c5b2ad9ca76fe0e8855
jonny rhea <jonathan.rhea@gmail.com>
2020-02-04 14:06:13 -0600

    example code for transfer ee

--- tests/ee_tests.rs
+++ tests/ee_tests.rs
@@ -1,17 +1,26 @@
+extern crate ease;
 use wrapper::{ee_code,transfer,build_state};
-use ease::{primitives,engine};
+use ease::primitives::address::Address;
+use ease::engine::Engine;
+
 
 #[test]
 fn test() {
     // TODO
 
+    let engine = Engine::new();
     // build initial state
     let state = build_state();
     // get generated ee wasm code
     let code = ee_code();
     // deploy code with state
-    // engine.deploy(code,state);
-
-
+    engine.deploy(code,state);
+    // create a tx
+    let tx = transfer("zero".as_bytes().to_vec(),"one".as_bytes().to_vec(),100,1);
+    // execute tx on node
+    engine.run(tx);
+    // update local state
+    //state.get_mut(Address::zero).set_balance(0);
+    //state.get_mut(Address::one).set_balance(200);
     assert_eq!(1+1, 2);
 }
