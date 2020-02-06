4d0c73e38346c77fc118f25f3aa0e572254b95a6
jonny rhea <jonathan.rhea@gmail.com>
2020-02-06 15:41:31 -0600

    changing dependency to quilt repo

--- ee/src/main.rs
+++ ee/src/main.rs
@@ -3,12 +3,9 @@
 
 use core::panic::PanicInfo;
 
-
-//#[cfg(feature = "wasm")]
 #[no_mangle]
 pub extern "C" fn main() {}
 
-//#[cfg(feature = "wasm")]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
